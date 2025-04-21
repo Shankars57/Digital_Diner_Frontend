cfd5a487a7cbca6100b93248dd153b6b101a2f63
MadCoder7 <shankarpubg4@gmail.com>
2025-04-21 20:11:01 +0530

    Initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./NavBar.css";
+import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 const NavBar = ({ setShowLogin }) => {
